1ddaa236cc15631b782d9c1f96a8ad4131520283
molengeek <saoussan.elk@gmail.com>
2025-05-19 23:24:05 +0200

    fin

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,14 +1,20 @@
 import './card.css'
-import glace from '../../assets/img/glace.jpg'
+import fleur1 from '../../assets/img/81705.jpg'
+import fleur2 from '../../assets/img/IMG_8714.jpg'
+import fleur3 from '../../assets/img/20210128052736_file_60124b48b207d_60124c20af9fd..avif'
+
 
 function Card({cardImg, name, price, stock, onBuy, argent}){
-    const imgG = glace
-
+    const images = { 
+        prod1: fleur1,
+        prod2: fleur2,
+        prod3: fleur3
+    }    
 
     return(
         <div id='divGloCard' className={stock === 1 ? 'limite' : stock === 0 ? 'out': 'full'}>
             <div className='divCardImg'>
-                <img src={imgG} alt="" />
+                <img src={images[name]} alt="" />
             </div>
             <div id='divCardInfos'>
                 <div className='divCardName'>
@@ -23,7 +29,7 @@ function Card({cardImg, name, price, stock, onBuy, argent}){
             </div>
             <div id='divCardBtn'>
                 <button onClick={onBuy} disabled={stock === 0 || argent < price}>
-                    {stock> 0 ? 'Acheter' : 'Article non disponible'}
+                    {stock===0 ? 'Article non disponible' : stock===1? 'Acheter le dernier' : 'Acheter'}
                 </button>
             </div>
         </div>
