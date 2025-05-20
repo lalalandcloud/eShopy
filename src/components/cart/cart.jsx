ce183d483f08c36c1a5c35a95ea740080fb2234c
molengeek <saoussan.elk@gmail.com>
2025-05-20 22:36:14 +0200

    miniatures

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,6 +1,17 @@
 import './cart.css'
+import fleur1 from '../../assets/img/5._Bouquet_d_exception_-_Printemps_-_Colore_-_Passion_1024x1024.jpg'
+import fleur2 from '../../assets/img/8401-xl@2x.jpg'
+import fleur3 from '../../assets/img/meadow.jpg'
+
 
 function Cart({argent, cb, remettre}){
+    const image ={
+        "Bouquet Champêtre": fleur1,
+        "Bouquet de Luxe": fleur2,
+        "Bouquet de Mariée": fleur3
+        
+    }
+
     // if (!cb || Object.values(cb).every(qty => qty === 0)){
     //     return null
     // }
@@ -17,6 +28,9 @@ function Cart({argent, cb, remettre}){
                     <ul id='ulCard'>
                         {Object.entries(cb || {}).map(([name, qty]) => (
                             <li key={name}>
+                                <div>
+                                    <img src={image[name]} alt="" />
+                                </div>
                                 {name} : <b>{qty}</b>
                                 <button
                                     onClick={() => remettre(name)}
